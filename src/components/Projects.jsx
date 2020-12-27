a23ee9d0eb42e8700a64c660cdc070d462a32da7
jonathan <jonathanfagan@pursuit.org>
2020-12-27 14:20:30 -0500

    commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,7 @@ import React from "react"
 // import { Route, Link, Switch } from 'react-router-dom'
 import NavBar from "./NavBar"
 import "../css/projects.css"
-import pic from "../pictures/sprite5.png"
+
 
 class Projects extends React.Component {
     constructor() {
