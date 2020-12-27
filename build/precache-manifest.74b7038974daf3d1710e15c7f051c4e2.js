9eccf7331be46464a921dc7e2ab3633babf0460b
jonathan <jonathanfagan@pursuit.org>
2020-12-27 14:11:30 -0500

    commit

---
+++ build/precache-manifest.74b7038974daf3d1710e15c7f051c4e2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc104aa7cbb5804ea1800e01a5f239ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "268a1c37a0ed1df3ff7a",
+    "url": "/static/css/main.41c620cc.chunk.css"
+  },
+  {
+    "revision": "49ca423d3c6c1c7575dc",
+    "url": "/static/js/2.2b1a9082.chunk.js"
+  },
+  {
+    "revision": "b4f9955a46235ab5d9673fafd3d3f6c5",
+    "url": "/static/js/2.2b1a9082.chunk.js.LICENSE"
+  },
+  {
+    "revision": "268a1c37a0ed1df3ff7a",
+    "url": "/static/js/main.5c1d0dda.chunk.js"
+  },
+  {
+    "revision": "b7d756d4614ae1d6fbe0",
+    "url": "/static/js/runtime-main.b9866801.js"
+  },
+  {
+    "revision": "1ce113a227e25d184ee040478788f701",
+    "url": "/static/media/IMG_0158.1ce113a2.jpg"
+  },
+  {
+    "revision": "6a381fc00f47d7a04fba5b2bad6841c4",
+    "url": "/static/media/c++_logo.6a381fc0.png"
+  },
+  {
+    "revision": "6eed9079ae1b7c07153d1c9479fa3003",
+    "url": "/static/media/csharp.6eed9079.png"
+  },
+  {
+    "revision": "482dce8fe2ffc91fd287d8118f3488c2",
+    "url": "/static/media/github-logo.482dce8f.png"
+  },
+  {
+    "revision": "85458a6717bed07d7db5146928d3cdd0",
+    "url": "/static/media/nodejs.85458a67.png"
+  },
+  {
+    "revision": "ea92d6580dda0fbcf61a8ccb16e0d02a",
+    "url": "/static/media/postgres.ea92d658.png"
+  },
+  {
+    "revision": "454acbc334c72cd2baeb6aee20c44e85",
+    "url": "/static/media/react_logo.454acbc3.png"
+  },
+  {
+    "revision": "c8f44df3b0bdbe3addb48058ed44c088",
+    "url": "/static/media/redux_logo.c8f44df3.png"
+  },
+  {
+    "revision": "819ad68d04f9cf8ecb768f34874f75c7",
+    "url": "/static/media/sprite5.819ad68d.png"
+  },
+  {
+    "revision": "948483a59b4f56937328518c40854c06",
+    "url": "/static/media/unity2.948483a5.png"
+  }
+]);
